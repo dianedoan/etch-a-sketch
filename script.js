8ed046256d5d29ba5a9e3254b25c03acb203f6eb
dianedoan <Dianedd419@hotmail.com>
2025-12-23 13:05:02 -0700

    Add mouse hover effect using event listeners

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // create 16 x 16 grid of square divs inside container div 
-// use flexbox to make divs appear as a grid (don't use CSS grid)
 const container = document.querySelector("#container");
 
+// create 16 x 16 squares
 const gridSize = 16;
 const totalSquares = gridSize * gridSize;
 for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
-}
 
+    // add "hover" effect using event listeners
+    square.addEventListener("mouseenter", () => {
+        square.style.cssText = "background: black";
+    });
+}
