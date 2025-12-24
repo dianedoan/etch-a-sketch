59c125fa9001d55ef62bdfc7dfa3ca00086adca0
dianedoan <Dianedd419@hotmail.com>
2025-12-23 18:30:30 -0700

    Move square size calculation from css to javascript

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // create 16 x 16 grid of square divs inside container div 
 const container = document.querySelector("#container");
+const containerSize = 520;
+
+// calculate square width/height
+const gridSize = 16;
+const squareSize = containerSize / gridSize;
+const totalSquares = gridSize * gridSize; // n x n grid
 
 // create 16 x 16 squares
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
 for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    container.appendChild(square);
 
+    // set square width and height
+    square.style.width  = `${squareSize}px`;
+    square.style.height  = `${squareSize}px`;
+
+    container.appendChild(square);
+    
     // add "hover" effect using event listeners
     square.addEventListener("mouseenter", () => {
-        square.style.cssText = "background: black";
+        square.style.backgroundColor = "#000";
     });
 }
