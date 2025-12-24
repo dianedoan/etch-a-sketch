ad4e1f4d96834e7834a85e311b46396a4e84ac9d
dianedoan <Dianedd419@hotmail.com>
2025-12-23 18:35:13 -0700

    Move grid creation code into createGrid function

--- script.js
+++ script.js
@@ -2,24 +2,27 @@
 const container = document.querySelector("#container");
 const containerSize = 520;
 
-// calculate square width/height
-const gridSize = 16;
-const squareSize = containerSize / gridSize;
-const totalSquares = gridSize * gridSize; // n x n grid
-
-// create 16 x 16 squares
-for (let i = 0; i < totalSquares; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-
-    // set square width and height
-    square.style.width  = `${squareSize}px`;
-    square.style.height  = `${squareSize}px`;
-
-    container.appendChild(square);
+function createGrid(gridSize) {
+    // calculate square width/height
+    const squareSize = containerSize / gridSize;
+    const totalSquares = gridSize * gridSize; // n x n grid
     
-    // add "hover" effect using event listeners
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "#000";
-    });
+    // create n x n squares
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+    
+        // set square width and height
+        square.style.width  = `${squareSize}px`;
+        square.style.height  = `${squareSize}px`;
+    
+        container.appendChild(square);
+        
+        // add "hover" effect using event listeners
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "#000";
+        });
+    }
 }
+
+createGrid(16);
